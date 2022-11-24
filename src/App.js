6a12d61ba6cfe711a349f692db55bec7fc7a8c58
Alba Tyto <gperettig@gmail.com>
2022-11-24 19:57:41 -0300

    Punto 3 con profe, ok

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import './App.css'
-// import Card from './components/Card/Card.jsx'
 import Cards from './components/Cards/Cards.jsx'
 import Nav from './components/Nav/Nav.jsx'
 import { useState } from 'react';
-// import SearchBar from './components/SearchBar/SearchBar.jsx'
-// import characters, { Rick } from './data.js'
-// const example = {
-//   id: 0,
-//   name: 'Morty Smith',
-//   species: 'Human',
-//   gender: 'Male',
-//   image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
-// };
+import { Route, Routes } from 'react-router-dom';
+import About from './components/About/About';
 
 function App() {
-
   const [characters, setChar] = useState([]);
   function onSearch(character) {
     fetch(`https://rickandmortyapi.com/api/character/${character}`)
@@ -29,36 +20,21 @@ function App() {
   };
   function onClose(id) {
     setChar(characters.filter(char => char.id !== id));
-    // setChar(characters.splice(characters.indexOf(id), 1))=> Esta no funciuono
-  }
+  };
   return (
     <div className='App' style={{ padding: '25px' }}>
-      {/* <div className='Present'>
-        <Card
-          name={Rick.name}
-          species={Rick.species}
-          gender={Rick.gender}
-          image={Rick.image}
-          onClose={() => window.alert('Emulamos que se cierra la card')}
-        />
-      </div> */}
-      <hr />
-      <div>
-        <Cards
-          characters={characters} onClose={onClose}
-        />
-      </div>
-      <hr />
-      {/* <div>
-        <SearchBar
-          onSearch={(characterID) => window.alert(characterID)}
-        />
-      </div> */}
       <nav>
-        <Nav onSearch={onSearch} characters={characters} setChar={setChar} />
+        <Nav path='/:nav' onSearch={onSearch} characters={characters} setChar={setChar} />
       </nav>
+      <Routes>
+        <Route path='/home' element={<Cards
+          characters={characters} onClose={onClose}
+        />}/>
+        <Route path='/about' element={<About />}/>
+        {/* <Route path="/detail/:detailId" element={<Detail />}/> */}
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
 export default App
